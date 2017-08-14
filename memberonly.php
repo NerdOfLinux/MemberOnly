db0a7892f4dfc216a7870848ecfa2c1040638ec7
NerdOfLinux <golf@runningwind.com>
2017-08-14 10:03:11 -0500

    Update memberonly.php

--- memberonly.php
+++ memberonly.php
@@ -1,3 +1,36 @@
 <?php
-
+ /*
+  Plugin Name: Member Only Content
+  Plugin URI: https://gateblogs.com
+  Description: A simple plugin to create member only content
+  Version: 0.1
+  Author: NerdOfLinux
+  Author URI: https://gateblogs.com
+  License: MIT
+ */
+/* Create the function */
+function post_filter( $content ) {
+    /* Create categories that are member only*/
+    $categories = array(
+        'premium',
+    );
+    /* If the post is in the category */
+    if ( in_category( $categories ) ) {
+        /* If the user is logged in, then show the content*/
+        if ( is_user_logged_in() ) {
+            return $content;
+        /* Else tell the user to log in */
+        } else {
+            /* $link = get_the_permalink();
+            $link = str_replace(':', '%3A', $link);
+            $link = str_replace('/', '%2F', $link);
+            $content = "<p>Sorry, this post is only available to members. <a href=\"gateblogs.com/login?redirect_to=$link\">Sign in/Register</a></p>"; */
+            $content= "<p> Sorry, this post is member only. <p>"
+            return $content;
+        }
+    } else {
+         return $content;
+    }
+}
+add_filter( 'the_content', 'post_filter' );
 ?>
