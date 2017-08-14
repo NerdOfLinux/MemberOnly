ab65c0e373bbefd058143e2de338af7bb9b04daf
NerdOfLinux <golf@runningwind.com>
2017-08-14 10:41:42 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,3 +1,20 @@
 <?php
-
+  public function __construct() {
+    // Hook into the admin menu
+    add_action( 'admin_menu', array( $variables, 'settings_page' ) );
+  }
+  public function settings_page() {
+    //Create the menu item and page
+    $page_title = "Member Only Content Settings Page";
+    $menu_title = "Member Only Content";
+    $capability = "manage_options";
+    $slug = "member_only";
+    $callback = array( $variables, 'settings_page_content' );
+    $icon = 'dashicons-admin-plugins';
+    $position = 100;
+  }
+  /* Create teh page*/
+  public function settingts_page_content() {
+    echo "Test of settings page."; 
+  }
 ?>
