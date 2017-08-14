34db9f0d8955fadbe361a22fafa226264af455d3
NerdOfLinux <golf@runningwind.com>
2017-08-14 14:44:46 -0500

    Added redirect checkbox

--- memberonly.php
+++ memberonly.php
@@ -93,6 +93,8 @@ function post_filter( $content ) {
     if ( in_category( $categories ) ) {
      /* If the user is logged in, then show the content*/
      if ( is_user_logged_in() ) {
+         $test = get_option("redirect");
+         echo $test;
          return $content;
      /* Else tell the user to log in */
      } else {
