b645c3f2a9095a367037f7700fbd1bc1858cf7ca
NerdOfLinux <golf@runningwind.com>
2017-08-14 11:13:05 -0500

    Working on settings page

--- memberonly.php
+++ memberonly.php
@@ -27,31 +27,31 @@ class member_only {
     public function settings_page_content() {
         echo "Test of settings page."; 
     }
- /* Create the function */
- public function post_filter( $content ) {
-     /* Create categories that are member only*/
-     $categories = array(
-         'premium',
-     );
-     /* If the post is in the category */
-     if ( in_category( $categories ) ) {
-         /* If the user is logged in, then show the content*/
-         if ( is_user_logged_in() ) {
-             return $content;
-         /* Else tell the user to log in */
-         } else {
-             /* $link = get_the_permalink();
-             $link = str_replace(':', '%3A', $link);
-             $link = str_replace('/', '%2F', $link);
-             $content = "<p>Sorry, this post is only available to members. <a href=\"gateblogs.com/login?redirect_to=$link\">Sign in/Register</a></p>"; */
-             $content= "<p> Sorry, this post is member only. <p>";
-             return $content;
-         }
+}
+new member_only();
+add_filter( 'the_content', 'post_filter' );
+/* Create the function */
+function post_filter( $content ) {
+ /* Create categories that are member only*/
+ $categories = array(
+     'premium',
+ );
+ /* If the post is in the category */
+ if ( in_category( $categories ) ) {
+     /* If the user is logged in, then show the content*/
+     if ( is_user_logged_in() ) {
+         return $content;
+     /* Else tell the user to log in */
      } else {
-          return $content;
+         /* $link = get_the_permalink();
+         $link = str_replace(':', '%3A', $link);
+         $link = str_replace('/', '%2F', $link);
+         $content = "<p>Sorry, this post is only available to members. <a href=\"gateblogs.com/login?redirect_to=$link\">Sign in/Register</a></p>"; */
+         $content= "<p> Sorry, this post is member only. <p>";
+         return $content;
      }
+ } else {
+      return $content;
  }
 }
-/* add_filter( 'the_content', 'post_filter' ); */
-new member_only();
 ?>
