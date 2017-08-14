ec9366beb32804fcd4fb08a469dfc2886cb91375
NerdOfLinux <golf@runningwind.com>
2017-08-14 10:58:21 -0500

    Merged *.php

--- memberonly.php
+++ memberonly.php
@@ -9,8 +9,24 @@
   License: MIT
  */
 class member_only {
- /* Include settings.php */
-require("settings.php");
+    public function __construct() {
+        // Hook into the admin menu
+        add_action( 'admin_menu', array( $variables, 'settings_page' ) );
+    }
+    pubic function settings_page() {
+        //Create the menu item and page
+        $page_title = "Member Only Content Settings Page";
+        $menu_title = "Member Only Content";
+        $capability = "manage_options";
+        $slug = "member_only";
+        $callback = array( $variables, 'settings_page_content' );
+        $icon = 'dashicons-admin-plugins';
+        $position = 100;
+    }
+    /* Create the page*/
+    public function settings_page_content() {
+        echo "Test of settings page."; 
+    }
  add_filter( 'the_content', 'post_filter' );
  /* Create the function */
  public function post_filter( $content ) {
