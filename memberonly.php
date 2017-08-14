e2ca02b5347f08d4b337fa31e4d5e8f4a35449f5
NerdOfLinux <golf@runningwind.com>
2017-08-14 10:24:30 -0500

    Update memberonly.php

--- memberonly.php
+++ memberonly.php
@@ -9,31 +9,28 @@
   License: MIT
  */
 /* Create the function */
-class member_only {
- function post_filter( $content ) {
-     /* Create categories that are member only*/
-     $categories = array(
-         'premium',
-     );
-     /* If the post is in the category */
-     if ( in_category( $categories ) ) {
-         /* If the user is logged in, then show the content*/
-         if ( is_user_logged_in() ) {
-             return $content;
-         /* Else tell the user to log in */
-         } else {
-             /* $link = get_the_permalink();
-             $link = str_replace(':', '%3A', $link);
-             $link = str_replace('/', '%2F', $link);
-             $content = "<p>Sorry, this post is only available to members. <a href=\"gateblogs.com/login?redirect_to=$link\">Sign in/Register</a></p>"; */
-             $content= "<p> Sorry, this post is member only. <p>";
-             return $content;
-         }
-     } else {
-          return $content;
-     }
- } 
+function post_filter( $content ) {
+    /* Create categories that are member only*/
+    $categories = array(
+        'premium',
+    );
+    /* If the post is in the category */
+    if ( in_category( $categories ) ) {
+        /* If the user is logged in, then show the content*/
+        if ( is_user_logged_in() ) {
+            return $content;
+        /* Else tell the user to log in */
+        } else {
+            /* $link = get_the_permalink();
+            $link = str_replace(':', '%3A', $link);
+            $link = str_replace('/', '%2F', $link);
+            $content = "<p>Sorry, this post is only available to members. <a href=\"gateblogs.com/login?redirect_to=$link\">Sign in/Register</a></p>"; */
+            $content= "<p> Sorry, this post is member only. <p>";
+            return $content;
+        }
+    } else {
+         return $content;
+    }
 }
-new member_only();
 add_filter( 'the_content', 'post_filter' );
 ?>
