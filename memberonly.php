7cf4cdad827a4694f0e915b69dd9022a60b527b0
NerdOfLinux <golf@runningwind.com>
2017-08-14 11:29:57 -0500

    Working on settings page

--- memberonly.php
+++ memberonly.php
@@ -24,7 +24,7 @@ class member_only {
         $slug = "member_only";
         $callback = array( $variables, 'settings_page_content' );
         //add_submenu_page( $parent_slug, $page_title, $menu_title, $capability, $slug, $callback );
-        add_submenu_page("test", "test", "test", "test", "test", $callback);
+        add_submenu_page("test", "test", "test", "test", "test", $function = "settings_page_content");
     }
     /* Create the page*/
     public function settings_page_content() {
