e46a7a8af1bcd5bf5a811f101c1e83c54ea16062
NerdOfLinux <golf@runningwind.com>
2017-08-14 14:30:23 -0500

    Added redirect checkbox

--- memberonly.php
+++ memberonly.php
@@ -72,8 +72,8 @@
         if ( "categories" === $args[ 'context' ]){
                 echo "<input name=\"categories\" id=\"categories\" type=\"text\" value=\"" .get_option("categories"). "\"\>";
         }else if ( "loginURL" === $args['context']){
-            echo "<input name=\"loginURL\" id=\"loginURL\" type=\"text\" value=\"" .get_option("loginURL"). "\"\>"; }
-        else if{
+            echo "<input name=\"loginURL\" id=\"loginURL\" type=\"text\" value=\"" .get_option("loginURL"). "\"\>";
+        }else if ( "redirect" === $args['context']){
             echo "<input name=\"redirect\" id=\"redirect\" type=\"checkbox\" value=\"" .get_option("redirect"). "\"\>";
         }
     }
