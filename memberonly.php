56c4e3716508856a7795c7ed1f7d71f19d000a95
NerdOfLinux <golf@runningwind.com>
2017-08-14 13:26:06 -0500

    Adding/Updating input fields

--- memberonly.php
+++ memberonly.php
@@ -8,43 +8,36 @@
   Author URI: https://gateblogs.com
   License: MIT
  */
-class member_only {
-    /* Create blank array */
-    public function __construct() {
-        //$this = [];
-        // Hook into the admin menu
-        add_action( 'admin_menu', array( $this, 'settings_page' ) );
-        add_action( 'admin_init', array( $this, 'setup_sections' ) );
-        add_action( 'admin_init', array( $this, 'setup_fields' ) );
-    }
-    public function settings_page() {
-        //Create the menu item and page
-        $parent_slug = "member_only_fields";
-        $page_title = "Member Only Content Settings Page";
-        $menu_title = "Member Only Content";
-        $capability = "manage_options";
-        $slug = "member_only_fields";
-        $callback = array( $this, 'settings_page_content' );
-        add_submenu_page( "options-general.php", $page_title, $menu_title, $capability, $slug, $callback );
-    }
-    /* Create the page*/
-    public function settings_page_content() { ?>
-        <div class="wrap">
-            <h2> Member Only Content </h2>
-            <form method="post" action="options.php">
-                <?php
-                    settings_fields("member_only_fields");
-                    do_settings_sections("member_only_fields");
-                    submit_button();
-                ?>
-            </form>
-    <?php
-    }
-    /* Add options to settings page*/
-    public function setup_sections() {
-        add_settings_section("categories", "Member Only Categories: ", array($this, 'section_callback'), "member_only_fields");
-        add_settings_section("loginURL", "Login URL: ", array($this, 'section_callback'), "member_only_fields");
-    }
+    class Member_Only {
+        /* Create blank array */
+        public function __construct() {
+            //$this = [];
+            // Hook into the admin menu
+            add_action( 'admin_menu', array( $this, 'settings_page' ) );
+            add_action( 'admin_init', array( $this, 'setup_init' ) );
+        }
+        public function settings_page() {
+            //Create the menu item and page
+            $parent_slug = "member_only_fields";
+            $page_title = "Member Only Content Settings Page";
+            $menu_title = "Member Only Content";
+            $capability = "manage_options";
+            $slug = "member_only_fields";
+            $callback = array( $this, 'settings_page_content' );
+            add_submenu_page( "options-general.php", $page_title, $menu_title, $capability, $slug, $callback );
+        }
+        /* Create the page*/
+        public function settings_page_content() { ?>
+<div class="wrap">
+<h2> Member Only Content </h2>
+<form method="post" action="options.php">
+<?php
+    settings_fields("member_only_fields");
+    do_settings_sections("member_only_fields");
+    submit_button();
+    ?>
+</form>
+<?php }
     /* Setup section_callback */
     public function section_callback( $arguments ) {
         /* Set up input*/
@@ -54,18 +47,22 @@ class member_only {
                 break;
             case "loginURL":
                 echo "The login URL of your site. ";
-            break;
+                break;
         }
     }
-    public function setup_fields() {
+    public function setup_init() {
+        register_setting("member_only_fields", "categories");
+        
+        add_settings_section("categories", "Member Only Categories: ", array($this, 'section_callback'), "member_only_fields");
         add_settings_field( 'categories', 'Categories: ', array( $this, 'field_callback' ), 'member_only_fields', 'categories' );
+        
+        add_settings_section("loginURL", "Login URL: ", array($this, 'section_callback'), "member_only_fields");
     }
     /* Create input fields*/
-        public function field_callback ( $arguments ) {
-            echo "<input name=\"categories\" id=\"categories\" type=\"text\" value=\"" .get_option("categories"). "\"\>";
-            register_setting("member_only_fields", "categories");
-        }
-}
+    public function field_callback ( $arguments ) {
+        echo "<input name=\"categories\" id=\"categories\" type=\"text\" value=\"" .get_option("categories"). "\"\>";
+    }
+    }
 new member_only();
 add_filter( 'the_content', 'post_filter' );
 /* Create the function */
