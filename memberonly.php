105b966b70f483daad5046db06d86cdef36f31e5
NerdOfLinux <golf@runningwind.com>
2017-08-15 12:15:27 -0500

    Working on exit before breaking the site

--- memberonly.php
+++ memberonly.php
@@ -11,5 +11,5 @@
 /* Make sure there are no syntax errors before running
    from: https://stackoverflow.com/questions/1184628/php-equivalent-of-include-using-eval
  */
-eval('?>' . file_get_contents('memberonlycode.php') . '<?php;');
+eval('?>' . file_get_contents("memberonlycode.php") . '<?php;');
 ?>
